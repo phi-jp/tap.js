5b6be62abea6989dd94ff0bd57e9fcd5fe92a89e
phi <phi1618jp@gmail.com>
2015-12-09 12:54:04 +0900

    fix gulp

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+/*
+* gulpfile.js
+*/
+
+var gulp = require('gulp');
+var ghelper = require('gulp-helper');
+ghelper.require();
+
+var pkg = require('./package.json');
+
+var banner = [
+  "/* ",
+  " * <%= pkg.name %> <%= pkg.version %>",
+  " * <%= pkg.description %>",
+  " * MIT Licensed",
+  " * ",
+  " * Copyright (C) 2015 phi, <%= pkg.homepage %>",
+  " */",
+  "",
+  "",
+].join('\n');
+
+
+gulp.task('default', ['uglify']);
+
+gulp.task('uglify', function() {
+  return gulp.src('./tap.js')
+    .pipe(uglify())
+    .pipe(header(banner, {
+      pkg: pkg,
+    }))
+    .pipe(rename({
+      extname: '.min.js'
+    }))
+    .pipe(gulp.dest('./'))
+    .on('end', function() {
+      console.log('finish');
+    });
+});
